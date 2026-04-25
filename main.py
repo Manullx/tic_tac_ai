3fcff88a9781ff6a53ced4559393b7cbcca60bbd
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-04-25 17:46:26 -0300

    (back) feat: get game state

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 
-class Square( BaseModel ):
+class GameState( BaseModel ):
 
-    row: int
-    col: int
+    state: list
 
 
 app = FastAPI()
@@ -20,7 +20,18 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
+
+# @app.get("/")
+# def index():
+
+#     return HTMLResponse( open("src/index.html").read(), status_code = 200, media_type = "text/html" )
+
+
 @app.post("/square")
-async def square( sqr: Square ):
+async def square( game_state: GameState ):
     
+    req_body = game_state.model_dump()
+    state = req_body['state']
+
+
     return JSONResponse({ 'row': random.randint( 1, 3 ), 'col': random.randint(1, 3) })
