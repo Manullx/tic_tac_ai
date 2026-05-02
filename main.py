5dcf613d0e341a4a766a8e4c690aa560a3cbeda8
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-05-02 16:22:11 -0300

    (back) feat: database models

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 import random
+from typing import List
+from datetime import datetime
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from sqlmodel import Column, JSON, String, Field, SQLModel, Session, create_engine
 
 
+#Models
+class Game( SQLModel, table = True ):
+
+    id: int | None = Field( default = None, primary_key = True )
+    finished: bool
+    started_at:  datetime
+    finished_at: datetime | None
+
+
+class Play( SQLModel, table = True ):
+
+    game_id: int = Field( foreign_key = "game.id" )
+    id: int | None = Field( default = None, primary_key = True )
+    state: List[ str ] = Field( sa_column = Column( JSON ) )
+
+
+#Classes
 class GameState( BaseModel ):
 
     state: list
 
 
+#Functions
+def create_game() -> int:
+
+    game = Game( finished = False, started_at = datetime.now() )
+
+    session = Session( engine )
+
+    session.add( game )
+
+    session.commit()
+
+    return game.id
+    
+
+#Execution
+
+#DB
+db_name = "db.sqlite"
+db_uri = f"sqlite:///{db_name}"
+
+engine = create_engine( db_uri, echo = True )
+
+SQLModel.metadata.create_all( engine )
+
+#FastAPI
 app = FastAPI()
 
 app.add_middleware(
@@ -21,10 +66,19 @@ app.add_middleware(
 )
 
 
-# @app.get("/")
-# def index():
+#Routes
+@app.get("/")
+def index():
 
-#     return HTMLResponse( open("src/index.html").read(), status_code = 200, media_type = "text/html" )
+    return HTMLResponse( open("src/index.html").read(), status_code = 200, media_type = "text/html" )
+
+
+@app.get("/game")
+def game():
+    
+    game_id = create_game()
+    
+    return JSONResponse( { "game_id": game_id }, status_code = 200 )
 
 
 @app.post("/square")
