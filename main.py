85bd04e445250e751d271da7136db01665915198
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-06-17 21:25:51 -0300

    (back) feat: game rules final definitions

--- main.py
+++ main.py
@@ -3,31 +3,90 @@ from datetime import datetime
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, HTMLResponse
-from sqlmodel import Session
+from sqlmodel import Session, SQLModel, select
 
-from db import session, Game, Play
+from db import engine, Game, Play
+
+
+#Schemas
+class PlayRequest( SQLModel ):
+
+    game_id: int
+    row: int
+    col: int
+
 
 #Functions
 def create_game() -> int:
 
-    game = Game( finished = False, started_at = datetime.now() )
+    with Session( engine ) as session:
 
-    session.add( game )
-    session.commit()
+        game = Game( finished = False, started_at = datetime.now() )
 
-    return game.id
+        session.add( game )
+        session.commit()
+
+        return game.id
 
 
-def create_play( game_id: int, state: list ) -> None:
+def create_play( session: Session, game: Game, player: str, row: int, col: int ) -> None:
 
-    
-    play = Play( game_id = game_id, state = state )
-    
+    play = Play( game_id = game.id, play_n = len( game.plays ), player = player, row = row, col = col, reward = 0 )
+
     session.add( play )
     session.commit()
+    session.refresh( game )
 
     return
 
+
+def verify_win( game: Game ) -> str | None:
+
+    game_state = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
+
+    for p in game.plays:
+        
+        game_state[p.row][p.col] = p.player
+        
+    #Verify Lines
+    for row in game_state:
+        
+        if None in row:
+            continue
+        
+        if len( row_set := set(row) ) == 1:
+
+            return row_set.pop()
+    
+    #Verify Cols
+    for col_i in range(0, 3):
+        
+        col = [ row[col_i] for row in game_state  ]
+        
+        if None in col:
+            continue
+        
+        if len( col_set := set(col) ) == 1:
+
+            return col_set.pop()
+    
+    # #Verify Diagonals
+    first_diagonal = [ game_state[ij][ij] for ij in range(0, 3) ]
+    second_diagonal = [ game_state[ij][2 - ij] for ij in range(2, -1, -1) ]
+    if len( first_diagonal_set := set(first_diagonal) ) == 1:
+
+            return first_diagonal_set.pop()
+    
+    if len( second_diagonal_set := set(second_diagonal) ) == 1:
+
+            return second_diagonal_set.pop()
+    
+    return None
+    
 
 #FastAPI
 app = FastAPI()
@@ -43,23 +102,63 @@ app.add_middleware(
 #Routes
 @app.get("/")
 def index():
-
     return HTMLResponse( open("src/index.html").read(), status_code = 200, media_type = "text/html" )
 
 
 @app.get("/game")
 def game():
     
-    game_id = create_game()
+    game_id: int = create_game()
     
     return JSONResponse( { "game_id": game_id }, status_code = 200 )
 
 
 @app.post("/play")
-def play( play: Play ):
+def play( play: PlayRequest ):
     
-    game_id, state, id = play.model_dump().values()
-    
-    create_play( game_id, state )
+    game_id = play.game_id
+    player_row = play.row
+    player_col = play.col
 
-    return JSONResponse({ 'row': random.randint( 1, 3 ), 'col': random.randint(1, 3) })
+    with Session( engine ) as session:
+
+        query = select( Game ).where( Game.id == game_id)
+        result = session.exec( query )
+        game = result.first()
+        
+        create_play( session, game, player = "X", row = player_row, col = player_col )
+
+        if winner := verify_win( game ):
+            
+            game.finished = True
+            game.finished_at = datetime.now()
+            return JSONResponse({ 'finished': True, 'winner': winner })
+        
+        model_plays = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 1),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2)
+        ]
+        
+        for play in game.plays:
+            model_plays.remove( (play.row, play.col) )
+            
+        if len( model_plays ) == 0:
+            return JSONResponse({ 'finished': True, 'winner': None, 'draw': True })
+        
+        model_row, model_col = random.choice( model_plays )
+        
+        create_play( session, game, player = 'O', row = model_row, col = model_col )
+        if winner := verify_win( game ):
+            
+            game.finished = True
+            game.finished_at = datetime.now()
+            return JSONResponse({ 'finished': True, 'winner': winner, 'row': model_row, 'col': model_col  })
+            
+        return JSONResponse({ 'finished': False, 'winner': None, 'row': model_row, 'col': model_col })
