0008890e9fa1c8f9b4752e82b12a6ce873397c95
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-07-04 17:56:31 -0300

    (back) feat: evaluate game

--- rl/enviroment.py
+++ rl/enviroment.py
@@ -3,14 +3,14 @@ from sqlmodel import Session, select
 from db import Game
 
 
-def estimate_reward( game: Game, row: int, col: int ) -> None:
+def estimate_reward( current_game: Game, row: int, col: int ) -> None:
 
     reward = 0.1
     
-    row_formation = [ p.player for p in game.plays if p.row == row]
-    col_formation = [ p.player for p in game.plays if p.col == col ]
-    first_diagonal = [ p.player for p in game.plays if ( p.row, p.col ) in [(0, 0), (1, 1), (2, 2)] ] if ( row, col ) in [(0, 0), (1, 1), (2, 2)] else None
-    second_diagonal = [ p.player for p in game.plays if ( p.row, p.col ) in [(0, 2), (1, 1), (2, 0)] ] if ( row, col ) in [(0, 2), (1, 1), (2, 0)] else None
+    row_formation = [ p.player for p in current_game.plays if p.row == row]
+    col_formation = [ p.player for p in current_game.plays if p.col == col ]
+    first_diagonal = [ p.player for p in current_game.plays if ( p.row, p.col ) in [(0, 0), (1, 1), (2, 2)] ] if ( row, col ) in [(0, 0), (1, 1), (2, 2)] else None
+    second_diagonal = [ p.player for p in current_game.plays if ( p.row, p.col ) in [(0, 2), (1, 1), (2, 0)] ] if ( row, col ) in [(0, 2), (1, 1), (2, 0)] else None
 
     #Prevent player from winning
     reward += len([ formation for formation in [ row_formation, col_formation, first_diagonal, second_diagonal ] if formation == ["X", "X"]]) * 0.15
