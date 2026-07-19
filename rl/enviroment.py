1c1a2ecd0e6cb0a1d6a8bf7706c404db560c4100
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-07-19 17:22:49 -0300

    (back) feat: add winning rule to reward estimating

--- rl/enviroment.py
+++ rl/enviroment.py
@@ -1,11 +1,12 @@
 from sqlmodel import Session, select
 
-from db import Game
+from models.game import Game
+from models.play import Players
 
+def estimate_play_reward( current_game: Game, player: Players, row: int, col: int ) -> None:
 
-def estimate_reward( current_game: Game, row: int, col: int ) -> None:
-
-    reward = 0.0
+    reward = 0.1
+    opponent = Players.o if player == Players.x else Players.x
     
     row_formation = [ p.player for p in current_game.plays if p.row == row]
     col_formation = [ p.player for p in current_game.plays if p.col == col ]
@@ -13,9 +14,12 @@ def estimate_reward( current_game: Game, row: int, col: int ) -> None:
     second_diagonal = [ p.player for p in current_game.plays if ( p.row, p.col ) in [(0, 2), (1, 1), (2, 0)] ] if ( row, col ) in [(0, 2), (1, 1), (2, 0)] else None
 
     #Prevent player from winning
-    reward += len([ formation for formation in [ row_formation, col_formation, first_diagonal, second_diagonal ] if formation == ["X", "X"]]) * 0.15
+    reward += len([ formation for formation in [ row_formation, col_formation, first_diagonal, second_diagonal ] if formation == [opponent, opponent]]) * 0.15
     
     #Almost winning
-    reward += len([ formation for formation in [ row_formation, col_formation, first_diagonal, second_diagonal ] if formation == ["O"]]) * 0.3
+    reward += len([ formation for formation in [ row_formation, col_formation, first_diagonal, second_diagonal ] if formation == [player]]) * 0.3
+
+    #winning
+    reward += 1 if [player, player, player] in [row_formation, col_formation, first_diagonal, second_diagonal] else 0
 
     return reward
