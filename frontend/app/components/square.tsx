d2f70f94ad7cb2b29e58c3cf5ad3804046ce890d
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-07-22 16:10:16 -0300

    (front) feat: square structure

--- frontend/app/components/square.tsx
+++ frontend/app/components/square.tsx
@@ -8,16 +8,27 @@ export default function Square( {
     boardGameState: Array<Array<Play | null>>, squareRow: number, squareCol: number, handleClickSquare: any
   }) {
 
+    if ( boardGameState[squareRow][squareCol] == null ) {
+        
+        return (
+            <div 
+                className = "flex justify-center items-center w-20 h-20 bg-white m-1 rounded-lg text-xl font-bold"
+                onClick={ _ => handleClickSquare( squareRow, squareCol ) }
+            >
+            </div>
+        )
+    }
+    
     return (
         <div 
             className = "flex justify-center items-center w-20 h-20 bg-white m-1 rounded-lg text-xl font-bold border-2"
             style = {{
-                backgroundColor: boardGameState[squareRow][squareCol] == null ? "white" : boardGameState[squareRow][squareCol].player == "X" ? "#f0b5b5" : "#c1d2e2",
-                borderColor: boardGameState[squareRow][squareCol] == null ? "white" : boardGameState[squareRow][squareCol].player == "X" ? "#C24242" : "#4282C2"
+                backgroundColor: boardGameState[squareRow][squareCol].player == "X" ? "#f0b5b5" : "#c1d2e2",
+                borderColor: boardGameState[squareRow][squareCol].player == "X" ? "#C24242" : "#4282C2"
             }}
             onClick={ _ => handleClickSquare( squareRow, squareCol ) }
         >
-            { boardGameState[squareRow][squareCol] != null ? boardGameState[squareRow][squareCol].player : null  }
+            { boardGameState[squareRow][squareCol].player }
         </div>
     )
 } 
